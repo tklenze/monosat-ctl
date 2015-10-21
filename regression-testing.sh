8cc532675c8b6f870db3d6e1c459117b9a180861
tobias <tobiask@cs.ubc.ca>
2015-10-21 16:47:03 +0200

    regression testing now also points out Assertion violations, not just when the program computes an incorrect result

--- regression-testing.sh
+++ regression-testing.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 for filename in inputs/sat/*; do
-    ./Debug/monosat "$filename" 2> /dev/null | grep UNSAT
+    ./Debug/monosat "$filename" 2> /tmp/monosat-regression-test | grep UNSAT
     if [[ $? -ne 1 ]]; then
 	echo "REGRESSION PROBLEM"
 	echo "File $filename is supposed to be SAT, but is UNSAT"
     fi
+    cat /tmp/monosat-regression-test | grep -v "FPU to use double precision" # yeah, yeah, we know...
+    if [[ $? -ne 1 ]]; then
+	echo "REGRESSION PROBLEM"
+	echo "File $filename violates an Assertion"
+    fi
 done
 for filename in inputs/unsat/*; do
-    ./Debug/monosat "$filename" 2> /dev/null | grep SATISFIABLE | grep -v UNSAT
+    ./Debug/monosat "$filename" 2> /tmp/monosat-regression-test | grep SATISFIABLE | grep -v UNSAT
     if [[ $? -ne 1 ]]; then
 	echo "REGRESSION PROBLEM"
 	echo "File $filename is supposed to be UNSAT, but is SAT"
+    fi
+    cat /tmp/monosat-regression-test | grep -v "FPU to use double precision"
+    if [[ $? -ne 1 ]]; then
+	echo "REGRESSION PROBLEM"
+	echo "File $filename violates an Assertion"
     fi
 done
